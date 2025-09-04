24efb30fbbb72a913bf548f6528770a5230f2d5f
Yogesh Mali <yogeshmali11011@gmail.com>
2025-09-04 19:52:16 +0530

    final commit

--- app.js
+++ app.js
@@ -147,4 +147,8 @@ app.get('/logout', (req, res)=>{
     res.redirect('/login');
 })
 
-app.listen(3000);
+// app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
